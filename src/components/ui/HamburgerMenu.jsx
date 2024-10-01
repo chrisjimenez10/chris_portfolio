a04504782d936957112269200f2f0fdd09955022
Christopher Jimenez <jimenezchristopher5@gmail.com>
2024-09-30 22:07:33 -0500

    added tooltip feature UI

--- src/components/ui/HamburgerMenu.jsx
+++ src/components/ui/HamburgerMenu.jsx
@@ -12,8 +12,20 @@ const HamburgerMenu = ({navTitles, navIcons, classNames}) => {
   return (
     <div className={`${classNames || ""} fixed w-full bottom-0 h-[9vh] flex items-center justify-center bg-surface`}>
       <nav className="flex gap-10 z-50">
-        {navTitles.map(({icon, id, url})=>(
-          <Link key={id} to={url}>
+        {navTitles.map(({icon, id, url, title})=>(
+          <Link key={id} to={url} className="relative group">
+            {/* Tooltip on Hover */}
+            <div className="absolute -top-[2.5rem] hidden group-hover:flex">
+              <div className="bg-onSurface relative flex text-background items-center p-[5px] rounded-[5px]">
+                <div className="text-[12px]">
+                  {title}
+                </div>
+                {/* Triangle */}
+                <div className="border-solid border-l-white  border-y-transparent border-y-[6px] border-b-0"></div>
+              </div>
+            </div>
+
+            {/* Icon */}
             <div className={`rounded-xl ${pathname.pathname === url ? "bg-primary" : "bg-background/45"} p-3 group transition-all duration-300 hover:bg-primary`}>
               {icon}
             </div>

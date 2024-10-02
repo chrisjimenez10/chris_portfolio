3c0768cc367540fc8cde8df5b7c98d714c9381f8
Christopher Jimenez <jimenezchristopher5@gmail.com>
2024-10-02 11:31:45 -0500

    added opacity animation between icon transition and individual animations for each icons while on display

--- src/components/ui/ToggleThemeButton.jsx
+++ src/components/ui/ToggleThemeButton.jsx
@@ -1,6 +1,8 @@
+//Imports
 import { useState, useEffect } from "react";
 import { IoIosSunny } from "react-icons/io";
 import { BsMoonStarsFill } from "react-icons/bs";
+import { motion } from "framer-motion";
 
 const ToggleThemeButton = () => {
 
@@ -10,13 +12,39 @@ const ToggleThemeButton = () => {
   //Functions
   const handleIconToggle = () => {
     setIcon(icon === "sun" ? "moon" : "sun");
+  };
+
+  //Animation
+  const transitionVariants = {
+    initial: {
+      opacity: 0
+    },
+    animate: {
+      opacity: 1
+    }
   }
 
   return (
-    <div className="bg-onBackground/45 rounded-full flex items-center justify-center w-10 h-10">
-      <button className="" onClick={handleIconToggle}>
-        {icon === "sun" ? <IoIosSunny size={28} color="white" className="animate-spin" style={{animationDuration: "10s"}}/> : <BsMoonStarsFill size={24} color="white" className="animate-pulse" style={{animationDuration: "5s"}}/>}
-      </button>
+    <div className={`${icon === "sun" ? "bg-white" : "bg-black"} rounded-full w-10 h-10 cursor-pointer`} onClick={handleIconToggle}>       
+          <motion.div
+            variants={transitionVariants}
+            initial="initial"
+            animate={icon === "sun" ? "animate" : "initial"}
+            transition={{duration: 0.5, ease: "easeInOut"}}
+            className="relative top-[.34rem] left-[.38rem]"
+          >
+            <IoIosSunny size={28} color="black" className="animate-spin" style={{animationDuration: "15s"}}/>
+          </motion.div> 
+        
+          <motion.div
+          variants={transitionVariants}
+          initial="initial"
+          animate={icon === "sun" ? "initial" : "animate"}
+          transition={{duration: 0.5, ease: "easeInOut"}}
+          className="relative bottom-5 left-[.45rem]"
+          >
+            <BsMoonStarsFill size={24} color="white" className="animate-pulse" style={{animationDuration: "5s"}}/>
+          </motion.div>
     </div>
 
   )

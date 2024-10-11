8752eeca9b7e62525f1bde6d6441b781c4bbfa24
Christopher Jimenez <jimenezchristopher5@gmail.com>
2024-10-11 14:00:01 -0500

    refactored position of footer to fixed bottom and hover pulse effect on buttons in Home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = ({className}) => {
   return (
-    <footer className={`${className || ""} z-20 text-textColor/90 mt-10 md:mt-[4rem]`}>   
+    <footer className={`${className || ""} z-20 text-textColor/90`}>   
         <h1 className="text-center text-xs">
             {new Date().getFullYear()} © Christopher Jimenez. All Rights Reserved.
         </h1>  

f0f7f81fcce1023cb0529863b86dd7261013bc45
Christopher Jimenez <jimenezchristopher5@gmail.com>
2024-10-15 10:45:45 -0500

    added animation to components inside About page using framer motion and used window object to configure different animations based on screen size

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 //Imports
 import { aboutHeading, aboutDescription } from "../constants";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { ThemeContext } from "../App";
 import { motion } from "framer-motion";
 //UI
@@ -14,12 +14,66 @@ const About = () => {
   //Context
   const {theme} = useContext(ThemeContext);
 
+  //Screen Size (xl = 1280px)
+  const xl = 1280
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+  const handleResize = () => {
+    setScreenSize(window.innerWidth);
+  }
+  useEffect(()=>{
+    // NOTE: We are adding an event listener as means of updating screenSize state using the window object rather than updating state HERE and adding the state in the dependency array, so that we DON'T create an infinite loop of re-rendering and changing screen size infinitely
+    window.addEventListener("resize", handleResize);
+    return ()=> {window.removeEventListener("resize", handleResize)}; //Arrow function as a return statement to REMOVE event listener
+  },[]);
+
+  //Animation Variants
+  const topVariants = {
+    initial: {
+      opacity: 0,
+      x: "-100%"
+    },
+    animate: {
+      opacity: 1,
+      x: "0%"
+    }
+  };
+  const topLargeVariants = {
+    // Animation descending into view
+    initial: {
+      opacity: 0,
+      y: "-100%"
+    },
+    animate: {
+      opacity: 1,
+      y: "0%"
+    }
+  };
+
+  const bottomVariants = {
+    initial: {
+      opacity: 0,
+      x: "100%",
+    },
+    animate: {
+      opacity: 1,
+      x: "0%",
+    },
+  };
+
+
   return (
-    <Section id="about" classNames="md:mt-[7.8rem] 2xl:mt-[12rem]">
+    <Section id="about" classNames="md:mt-[7.8rem] 2xl:mt-[12rem] overflow-hidden">
       <HexagonParticlesContainer classNames="z-0" theme={theme}/>
       <div className="flex flex-col items-center justify-center p-5 gap-5 xl:flex-row xl:justify-around">
 
-        <div className={`flex flex-col gap-4 items-center justify-center lg:w-[750px] xl:gap-8 lg:text-center xl:text-start xl:items-start xl:pl-10 z-10 ${theme === "dark" ? "bg-hexBg/65 rounded-2xl p-2" : "bg-background/75"}`}>
+        <motion.div
+        id="top"
+        variants={screenSize >= xl ? topLargeVariants : topVariants}
+        whileInView="animate"
+        initial="initial"
+        animate="animate"
+        transition={{delay: 0.2, duration: 2.5, ease: "easeInOut"}}
+        className={`flex flex-col gap-4 items-center justify-center lg:w-[750px] xl:gap-8 lg:text-center xl:text-start xl:items-start xl:pl-10 z-10 ${theme === "dark" ? "bg-hexBg/65 rounded-2xl p-2" : "bg-background/75"}`}>
           <h1 className="text-textColor text-3xl lg:text-5xl font-bold">
             <span className="text-primary">{aboutHeading.substring(0, 27)}</span>
             <span>{aboutHeading.substring(27, 50)}</span>
@@ -30,10 +84,18 @@ const About = () => {
             <span>{aboutDescription.substring(23, 494)}</span>
           </h2>
           <DynamicStats classNames="flex mt-[2rem] sm:mt-10 xl:mt-0"/>
-        </div>
+        </motion.div>
 
-        <CredentialsContainer classNames="mt-[2rem] sm:mt-[3rem] xl:w-[650px] xl:h-[400px] z-10" theme={theme}/>
-        
+        <motion.div 
+        id="bottom"
+        variants={bottomVariants}
+        whileInView="animate"
+        initial="initial"
+        animate="animate"
+        transition={{delay: 0.2, duration: 2.5, ease: "easeInOut"}}
+        className="z-10 ">
+          <CredentialsContainer classNames="mt-[2rem] sm:mt-[3rem] xl:w-[650px] xl:h-[400px] z-10" theme={theme}/>
+        </motion.div>
         {/* Expand BOTTOM for mobile device screen sizes, so that bottom nav bar DOES NOT cover content */}
         <div className="h-[100px] w-full md:hidden"/>
 

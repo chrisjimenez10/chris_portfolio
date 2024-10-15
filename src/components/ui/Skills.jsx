5450f6c1a03fb607368052d94ac17bd50d82d748
Christopher Jimenez <jimenezchristopher5@gmail.com>
2024-10-15 11:55:52 -0500

    added animations to skills component with intricate and individual fade in sequential for icons

--- src/components/ui/Skills.jsx
+++ src/components/ui/Skills.jsx
@@ -1,12 +1,34 @@
 //Imports
 import { useState } from "react";
 import { backendDevelopment, devOps, webDevelopment, uiuxDesign } from "../../constants";
+import { motion } from "framer-motion";
 
 
 const Skills = () => {
 
     //State
     const [skills, setSkills] = useState("frontend");
+
+    //Animation Variants
+    const titlesVariants = {
+        initial: {
+            opacity: 0,
+            y: "-50%",
+        },
+        animate: {
+            opacity: 1,
+            y: "0%",
+        }
+    };
+
+    const iconsVariants = {
+        initial: {
+            opacity: 0,
+        },
+        animate: {
+            opacity: 1,
+        }
+    };
 
 
   return (
@@ -21,52 +43,88 @@ const Skills = () => {
         {skills === "frontend" && (
             <div className="h-[250px] flex flex-col gap-8">
                 <div className="flex flex-col gap-2">
-                    <h2 className='text-center'>Web Development</h2>
-                    <div className="flex flex-wrap gap-5 items-center justify-center">
+                    <motion.h2 className='text-center' variants={titlesVariants} initial="initial" animate="animate" 
+                    transition={{delay: 0, duration: 1, ease: "easeInOut"}}>Web Development</motion.h2>
+                    <motion.div 
+                    initial="initial"
+                    whileInView="animate"
+                    transition={{delayChildren: 0.2, staggerChildren: 0.2}}
+                    className="flex flex-wrap gap-5 items-center justify-center"
+                    >
                         {webDevelopment.map(({id, title, image})=>(
-                            <div key={id}>
+                            <motion.div 
+                            key={id}
+                            variants={iconsVariants}
+                            transition={{duration: 1}}
+                            >
                                 <img src={image} alt={title} width={40} height={40}/>
-                            </div>
+                            </motion.div>
                         ))}
-                    </div>
+                    </motion.div>
                 </div>
 
                 <div className="flex flex-col gap-2">
-                    <h2 className='text-center'>UI/UX Design</h2>
-                    <div className="flex flex-wrap gap-5 items-center justify-center">
+                    <motion.h2 className='text-center' variants={titlesVariants} initial="initial" animate="animate" 
+                    transition={{delay: 0, duration: 1, ease: "easeInOut"}}>UI/UX Design</motion.h2>
+                    <motion.div 
+                    initial="initial"
+                    whileInView="animate"
+                    transition={{delayChildren: 0.2, staggerChildren: 0.2}}
+                    className="flex flex-wrap gap-5 items-center justify-center"
+                    >
                         {uiuxDesign.map(({id, title, image})=>(
-                            <div key={id}>
+                            <motion.div 
+                            key={id}
+                            variants={iconsVariants}
+                            transition={{duration: 1}}
+                            >
                                 <img src={image} alt={title} width={40} height={40}/>
-                            </div>
+                            </motion.div>
                         ))}
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         )}
 
         {skills === "backend" && (
             <div className="h-[250px] flex flex-col gap-3">
-                <h2 className='text-center'>Backend Development</h2>
-                <div className="flex flex-wrap gap-8 items-center justify-center">
+                <motion.h2 className='text-center' variants={titlesVariants} initial="initial" animate="animate" 
+                    transition={{delay: 0, duration: 1, ease: "easeInOut"}}>Backend Development</motion.h2>
+                <motion.div className="flex flex-wrap gap-8 items-center justify-center"
+                initial="initial"
+                whileInView="animate"
+                transition={{delayChildren: 0.2, staggerChildren: 0.2}}
+                >
                     {backendDevelopment.map(({id, title, image, bg})=>(
-                        <div key={id} className={`${bg ? "bg-white rounded-md px-[2px]" : ""}`}>
+                        <motion.div key={id} className={`${bg ? "bg-white rounded-md px-[2px]" : ""}`}
+                        variants={iconsVariants}
+                        transition={{duration: 1}}
+                        >
                             <img src={image} alt={title} width={40} height={40} />
-                        </div>
+                        </motion.div>
                     ))}
-                </div>
+                </motion.div>
             </div>          
         )}
 
         {skills === "devops" && (            
             <div className="h-[250px] flex flex-col gap-3">
-                <h2 className='text-center'>DevOps</h2>
-                <div className="flex flex-wrap gap-8 items-center justify-center">
+                <motion.h2 className='text-center' variants={titlesVariants} initial="initial" animate="animate" 
+                    transition={{delay: 0, duration: 1, ease: "easeInOut"}}>DevOps</motion.h2>
+                <motion.div className="flex flex-wrap gap-8 items-center justify-center"
+                initial="initial"
+                whileInView="animate"
+                transition={{delayChildren: 0.2, staggerChildren: 0.2}}
+                >
                     {devOps.map(({id, title, image, bg})=>(
-                        <div key={id} className={`${bg ? "bg-white rounded-md px-[2px]" : ""}`}>
+                        <motion.div key={id} className={`${bg ? "bg-white rounded-md px-[2px]" : ""}`}
+                        variants={iconsVariants}
+                        transition={{duration: 1}}
+                        >
                             <img src={image} alt={title} width={40} height={40}/>
-                        </div>
+                        </motion.div>
                     ))}
-                </div>
+                </motion.div>
             </div>           
         )}
     </div>

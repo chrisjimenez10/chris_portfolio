e2b46be62abfbb965ad4d3342faf47666beee5e7
Christopher Jimenez <jimenezchristopher5@gmail.com>
2024-10-17 21:33:56 -0500

    added pulsating border to cards, hover effect, and found satisfying size scaling display for all screen sizes

--- src/components/ui/Cards.jsx
+++ src/components/ui/Cards.jsx
@@ -2,13 +2,37 @@ import React from 'react'
 
 const Cards = ({title, style, colorVariants, projects}) => {
 
+//Border Style
+  let singleBorder = "";
+  let bgGradient = "";
+  switch(style){
+    case "primary":
+      singleBorder = "border-primary/35";
+      bgGradient = "blueGradient";
+      break;
+    case "red":
+      singleBorder = "border-red/35";
+      bgGradient = "redGradient";
+
+      break;
+    case "green":
+      singleBorder = "border-green/35";
+      bgGradient = "greenGradient";
+      break;
+    default:
+      singleBorder = "border-contrastBg/35";
+      bgGradient = "defaultGradient";
+
+  };
+
   return (
     <div className="flex flex-wrap items-center justify-center gap-x-24 gap-y-5">
         {projects.map(({id, title, image, liveSite, gitHub})=>(
-            <div key={id} className={`h-[32rem] flex items-start justify-center sm:w-[570px] w-[80vw] ${colorVariants[style].bg} rounded-2xl transition-all hover:scale-105`}>
-                <div className="relative flex items-center justify-center sm:w-[590px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] m-5 p-5 bg-black rounded-2xl">
-                    <div className={`relative w-full h-full overflow-hidden lg:rounded-3xl bg-black`}>
-                        <img src={image} alt={title} className="absolute bottom-0 rounded-2xl px-5"/>                       
+            <div key={id} className={`h-[26rem] sm:h-[32rem] lg:h-[38rem] relative flex items-start justify-center rounded-2xl transition-all hover:scale-105`}>
+                <div className={`absolute inset-0 border-[.25rem] rounded-xl ${singleBorder} animate-pulse`} style={{animationDuration: "1.2s"}}/>
+                <div className="relative flex items-center justify-center sm:w-[590px] w-[80vw] overflow-hidden sm:h-[35vh] h-[25vh] m-3 rounded-xl">
+                    <div className={`relative w-full h-full overflow-hidden rounded-xl ${bgGradient}`}>
+                        <img src={image} alt={title} className="absolute bottom-0 transition-all px-2 rounded-xl"/>                       
                     </div>
                 </div>
             </div>
